13289ea2258d953b01471acd116d4050e3b3c3b4
Raymond S Brand <rsbx@acm.org>
2016-02-12 10:51:02 -0500

    Add copyright and fix perl warning.

--- schema_parse.pl
+++ schema_parse.pl
@@ -1,5 +1,41 @@
 #!/usr/bin/perl
 
+#  Copyright (c) 2013-2016, Raymond S Brand
+#  All rights reserved.
+#
+#  Redistribution and use in source and binary forms, with or without
+#  modification, are permitted provided that the following conditions
+#  are met:
+#
+#   * Redistributions of source code must retain the above copyright
+#     notice, this list of conditions and the following disclaimer.
+#
+#   * Redistributions in binary form must reproduce the above copyright
+#     notice, this list of conditions and the following disclaimer in
+#     the documentation and/or other materials provided with the
+#     distribution.
+#
+#   * Redistributions in source or binary form must carry prominent
+#     notices of any modifications.
+#
+#   * Neither the name of Raymond S Brand nor the names of its other
+#     contributors may be used to endorse or promote products derived
+#     from this software without specific prior written permission.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+#  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+#  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+#  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+#  COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+#  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+#  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+#  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+#  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+#  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+#  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+#  POSSIBILITY OF SUCH DAMAGE.
+
+
 use 5.10.1;
 use strict;
 use warnings;
@@ -325,7 +361,7 @@ while (@ARGV)
 			}
 		else
 			{
-			say $item;
+#			say $item;
 			}
 		}
 	close($fh);
@@ -478,7 +514,7 @@ sub inheritance_loop
 			my $supers = GetSupers($item->{'SUP'}, $type);
 			if (defined $supers)
 				{
-				$item->{'-TC_supers'} = [sort keys {map { (lc($_) => undef) } @{$supers}, $item->{'OID'}}];
+				$item->{'-TC_supers'} = [sort keys %{{map { (lc($_) => undef) } @{$supers}, $item->{'OID'}}}];
 				$progress = 1;
 				next;
 				}
@@ -543,7 +579,7 @@ for my $name (sort {$a cmp $b} keys %names)
 	{
 	for my $item (@{$names{$name}})
 		{
-#		next if $item->{'OID'} =~ /^\d.*\d$/;
+		next if $item->{'OID'} =~ /^\d.*\d$/;
 		say "'$name' ", $item->{'-Type'}, " ", ,$item->{'OID'};
 		}
 	}
